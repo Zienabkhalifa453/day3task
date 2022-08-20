541317c447ffb3c163b44f49c90aba3cb8df7f59
Zienabkhalifa453 <92791041+Zienabkhalifa453@users.noreply.github.com>
2022-08-20 04:19:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp2
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("enter a series of numbers separated by comma");
+            var num = Console.ReadLine();
+            var n = num.Split(",");
+            var max = Convert.ToInt32(n[0]);
+
+            foreach (var str in n)
+            {
+                var number = Convert.ToInt32(str);
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            Console.WriteLine("Max is: max");
+
+
+        }
+    }
+}
